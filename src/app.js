b2191d8fe25ba4b67acc225e7402a49e16e083bb
natyal1 <nataliaalpuy1@gmail.com>
2023-11-24 12:26:29 -0300

    se crea const port para proceso deploy

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express=require ("express");
 const app = express ();
 const path = require("path");
 
-
-app.listen(3030, () => console.log ("Servidor corriendo en : http//3030/"));
+const port = process.env.PORT||3030;
+app.listen(port, () => 
+console.log (`Servidor corriendo en el puerto http://localhost:${port}`)
+);
 
 // debo escribir lo que el servidor debe responder cuando le llega una peticion o req
 
